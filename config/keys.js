5f197e9b02fb33fbcc217109ac603539e65ac848
Brian Bycynski <high5bri@hotmail.com>
2019-04-15 11:39:43 -0400

    Finished section 3

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
   mongoURI:
-    'mongodb://bdog72:bdog72@ds139896.mlab.com:39896/devconnector-bdog72-4-15-19'
+    'mongodb://bdog72:bdog72@ds139896.mlab.com:39896/devconnector-bdog72-4-15-19',
+  secretOrKey: 'secret'
 };
